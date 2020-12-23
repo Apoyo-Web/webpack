bff7639f56db96fb9ba5171875e5f82157c5eae0
Apoyo Web <mpienso@gmail.com>
2020-12-23 13:06:37 +0100

    Configuracion inicial webpack

---
+++ webpack.prod.js
@@ -0,0 +1,103 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+
+module.exports = {
+
+    mode: 'production', //Desarrollo o production
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
+    output: {
+        filename: 'main.[hash].js'
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: [
+                    "babel-loader",
+
+                ]
+            },
+
+
+
+
+            {
+
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+
+            },
+
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+
+            },
+
+
+
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false,
+                },
+            },
+
+            {
+                test: /\.(png|svs|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+
+
+
+
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+
+            patterns: [{
+                from: 'src/assets',
+                to: './assets/'
+            }]
+
+        }),
+
+        new MinifyPlugin(),
+        new CleanWebpackPlugin()
+
+    ]
+
+
+}
